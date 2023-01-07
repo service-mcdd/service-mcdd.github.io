a261a306d4f212ffbdea15f0c6d0f39607cc7149
service-mcdd <121069833+service-mcdd@users.noreply.github.com>
2023-01-07 12:23:13 +0800

    Update login.js

--- Login/login.js
+++ Login/login.js
@@ -16,7 +16,7 @@ function login() {
  
     } else if(username.value == "administrator" && pass.value == "minecraft666"){
  
-        window.location.href="https://service-mcdd.github.io/Login/welcome.html";
+        top.location="https://service-mcdd.github.io/Login/welcome.html";
  
     } else {
  
